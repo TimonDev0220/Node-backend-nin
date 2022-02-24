34d2e4d4337af82661d97344cbdfa40e4afab7d0
unknown <wiliwork1103@gmail.com>
2022-02-24 22:37:28 +0000

    implement models part

---
+++ models/Tickets.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const TicketSchema = new Schema({
+    ticket_name: {
+        type: String,
+        required: true,
+    },
+    ticket_description: {
+        type: String,
+        required: true,
+    },
+    ticket_price: {
+        type: String,
+        required: true,
+    },
+    ticket_deadline: {
+        type: String,
+        required: true,
+    },
+    ticket_skills: {
+        type: String,
+        required: true,
+    },
+    ticket_status: {
+        type: String,
+    },
+    ticket_winner: {
+        type: String,
+    },
+    ticket_budget: {
+        type: Number,
+    }
+});
+
+module.exports = Tickets = mongoose.model("Tickets", TicketSchema);
